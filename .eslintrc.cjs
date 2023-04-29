99783b10de94de430cda5a0d127fa6f7e67ecf56
Jeremy Cleland <jeremy@clelandco.com>
2023-04-29 03:00:08 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true, jest: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: "18.2" } },
+
+  plugins: ["react-refresh", "react", "import", "jsx-a11y"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <header className="color md:font-kola h-20 bg-stone-950 text-center text-5xl text-gray-50">
+        Hello
+      </header>
+    </>
+  );
+}
+
+export default App;
