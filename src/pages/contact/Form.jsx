01f717fc3421e4dc883702c23d819b29033b8b74
Jeremy Cleland <jeremy@clelandco.com>
2023-06-07 13:48:40 -0400

    add form validation

--- src/pages/contact/Form.jsx
+++ src/pages/contact/Form.jsx
@@ -1,47 +1,110 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  // Handle input change and update form data
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form data processing here
+    console.log("Form Data:", formData);
+
+    // Reset the form after submission
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
+  // Validate form data before submission
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    // Validate name field
+    if (!formData.name) {
+      errors.name = "Name is required";
+      isValid = false;
+    }
+
+    // Validate email field
+    if (!formData.email) {
+      errors.email = "Email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email is invalid";
+      isValid = false;
+    }
+
+    // Validate message field
+    if (!formData.message) {
+      errors.message = "Message is required";
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
   return (
-    <div className="container">
-      <form name="contact" method="POST">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="input">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Your Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="input">
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input">
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Your Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
+    <div className="sflex container mx-auto items-center justify-center font-Fira text-dark-900 dark:text-dark-100">
+      <div className="rounded-lg bg-dark-100 p-10 dark:bg-dark-900">
+        <form name="contact" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="input">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Your Message"
+              value={formData.message}
+              onChange={handleInputChange}
+            ></textarea>
+          </div>
+          <button type="submit" onClick={validateForm}>
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
