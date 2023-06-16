f8173c8aeedbcbfd649416e4902b6aa47cb15e6c
Jeremy Cleland <jeremy@clelandco.com>
2023-06-16 16:38:34 -0400

    add project three

--- src/data/portfolioData.jsx
+++ src/data/portfolioData.jsx
@@ -2,6 +2,7 @@
 import image1 from "../assets/images/portfolio/project-1/1.jpg";
 import image3 from "../assets/images/portfolio/project-1/2.png";
 import image2 from "../assets/images/portfolio/project-2/1.png";
+import image4 from "../assets/images/portfolio/project-3/expense-tracker1.png";
 import card1 from "../assets/images/portfolio/project-1/card-1.jpg";
 import card2 from "../assets/images/portfolio/project-1/card-2.jpg";
 import card3 from "../assets/images/portfolio/project-2/card-3.jpg";
@@ -29,11 +30,11 @@ export const portfolioData = [
   {
     id: 3,
     name: "ProjectThree",
-    title: "Battle of the Minds",
-    category: "UI/UX Design",
+    title: "Expense Tracking App",
+    category: "Web Application",
     description:
-      "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
-    img: image3,
+      "Web Application that allows users to track their expenses. Users can add, edit, and delete expenses. The app also displays a chart that shows the percentage of expenses for each category.",
+    img: image4,
     cardimg: card2,
   },
   {
