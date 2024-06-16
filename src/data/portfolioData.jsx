a7afe56027ac7c66fe65c5740932015cdfa946b1
Jeremy Cleland <jeremy@clelandco.com>
2024-06-15 23:18:52 -0400

    update portfolio

--- src/data/portfolioData.jsx
+++ src/data/portfolioData.jsx
@@ -32,31 +32,31 @@ export const portfolioData = [
       "Web Application that allows users to track their expenses. Users can add, edit, and delete expenses. The app also displays a chart that shows the percentage of expenses for each category.",
     img: image3,
   },
-  {
-    id: 4,
-    name: "ProjectFour",
-    title: "Project 4",
-    category: "UI/UX Design",
-    description:
-      "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
-    img: image3,
-  },
-  {
-    id: 5,
-    name: "ProjectFive",
-    title: "Project 5",
-    category: "UI/UX Design",
-    description:
-      "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
-    img: image3,
-  },
-  {
-    id: 6,
-    name: "ProjectSix",
-    title: "Project 6",
-    category: "Web Application",
-    description:
-      "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
-    img: image3,
-  },
+  // {
+  //   id: 4,
+  //   name: "ProjectFour",
+  //   title: "Project 4",
+  //   category: "UI/UX Design",
+  //   description:
+  //     "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
+  //   img: image3,
+  // },
+  // {
+  //   id: 5,
+  //   name: "ProjectFive",
+  //   title: "Project 5",
+  //   category: "UI/UX Design",
+  //   description:
+  //     "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
+  //   img: image3,
+  // },
+  // {
+  //   id: 6,
+  //   name: "ProjectSix",
+  //   title: "Project 6",
+  //   category: "Web Application",
+  //   description:
+  //     "Web application to fetch data from an API and display it in a user-friendly way by pretty printing the JSON. The application can perform GET, POST, PUT, and DELETE requests to the API. The application also has a history feature that allows the user to view the history of their requests",
+  //   img: image3,
+  // },
 ];
