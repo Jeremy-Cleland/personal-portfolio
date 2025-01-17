e702e54f70a0dab2652e1ee84fe1581f0e69ab81
Jeremy Cleland <jdcl@umich.edu>
2025-01-16 22:32:26 -0500

    updated skills

--- src/pages/resume/SkillSection.jsx
+++ src/pages/resume/SkillSection.jsx
@@ -11,9 +11,8 @@ const SkillsSection = () => {
     <div>
       <div className="mx-auto my-10 max-w-7xl">
         <div className="my-5 border-b-[1px] border-violet-400/60 py-5 text-center font-SourceCodePro text-dark-900 dark:text-gray-200">
-          <h4 className="text-center font-Fira text-lg tracking-tight md:text-left ">
+          <h4 className="text-center font-Fira text-lg tracking-tight md:text-left">
             <span className="text-base text-violet-400"> {">"} </span> Languages
-            & Tools
           </h4>
         </div>
 
@@ -21,17 +20,24 @@ const SkillsSection = () => {
           variants={staggerContainerVariant}
           initial="hidden"
           animate="show"
-          className="mx-auto grid max-w-6xl grid-cols-3 gap-1 md:grid-cols-6"
+          className="mx-auto mb-8 grid max-w-6xl grid-cols-2 gap-4 sm:grid-cols-4"
         >
           {resume.technicalSkills.languages.map((skill, index) => (
             <Skill key={index} icon={skill.icon} name={skill.name} />
           ))}
         </m.ul>
+
+        <div className="my-5 border-b-[1px] border-violet-400/60 py-5 text-center font-SourceCodePro text-dark-900 dark:text-gray-200">
+          <h4 className="text-center font-Fira text-lg tracking-tight md:text-left">
+            <span className="text-base text-violet-400"> {">"} </span> Tools & Frameworks
+          </h4>
+        </div>
+
         <m.ul
           variants={staggerContainerVariant}
           initial="hidden"
           animate="show"
-          className="mx-auto grid max-w-6xl grid-cols-3 gap-1 md:grid-cols-6"
+          className="mx-auto grid max-w-6xl grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-6"
         >
           {resume.technicalSkills.skillsAndTools.map((skill, index) => (
             <Skill key={index} icon={skill.icon} name={skill.name} />
