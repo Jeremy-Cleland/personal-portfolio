0958c3991bc404b398e9c054fbebf166e740c353
Jeremy Cleland <jdcl@umich.edu>
2025-04-18 15:32:40 -0400

    Remove cssnano configuration from PostCSS setup to simplify build process

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,5 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: process.env.NODE_ENV === 'production' ? {
-      preset: ['advanced', {
-        discardComments: { removeAll: true },
-        normalizeWhitespace: false,
-        colormin: true,
-        reduceIdents: false, // Prevents breaking animations
-      }]
-    } : false
   },
 };
